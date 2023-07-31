f4e606359d05a737b9c47fbd64ca73085828236b
Guineapeeg <marymarylinux@gmail.com>
2023-07-30 23:12:29 -0700

    us

--- myls.c
+++ myls.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 char* options;
 char* fileList;
+
+void* printIndexNumber();
 
 int main(int argc, char* argv[]){
 
@@ -11,8 +14,31 @@ int main(int argc, char* argv[]){
 
     }
 
-struct dirent *d;
-DIR *dp = opendir(".");
-
     options = "-i"; //print the index numberof each file
+    printIndexNumber(".");
+}
+
+void* printIndexNumber(char* locationOf){
+
+    struct dirent *d;
+    DIR *dp = opendir(locationOf);
+    struct stat myStat;
+
+
+    if(dp){
+
+        while ((d = readdir(dp)) != NULL)
+        {
+            if(d->d_name[0] != '.'){
+            stat(d->d_name, &myStat);
+            printf("%s ", d->d_name);
+            printf("%ld\n", myStat.st_ino);
+            }
+        }
+        closedir(dp);
+
+    }
+
+    return NULL;
+
 }
